16c9798b19f9f503c42b7d101bb598346768589f
tmvf <tmvf@yandex.ru>
2017-05-26 00:02:25 +0400

    service

--- src/main/java/service/AddressService.java
+++ src/main/java/service/AddressService.java
@@ -4,21 +4,74 @@ import bl.Util;
 import dao.AddressDAO;
 import entity.Address;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddressService extends Util implements AddressDAO {
 
-    Connection connection=getConnection();
+    Connection connection = getConnection();
 
     @Override
-    public void add(Address address) {
+    public void add(Address address) throws SQLException {
+        PreparedStatement preparedStatement = null;
+
+        String sql = "insert inti address(id,country,street,post_code) values(?,?,?,?,?)";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, address.getId());
+            preparedStatement.setString(2, address.getCountry());
+            preparedStatement.setString(3, address.getCity());
+            preparedStatement.setString(4, address.getStreet());
+            preparedStatement.setString(5, address.getPostCode());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
 
     }
 
     @Override
-    public List<Address> getAll() {
-        return null;
+    public List<Address> getAll() throws SQLException {
+        List<Address> addressList = new ArrayList<>();
+
+        String sql = "select id,country,street,post_code from address";
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Address address = new Address();
+                address.setId(resultSet.getLong("id"));
+                address.setCountry(resultSet.getString("country"));
+                address.setCity(resultSet.getString("city"));
+                address.setStreet(resultSet.getString("street"));
+                address.setPostCode(resultSet.getString("post_code"));
+
+                addressList.add(address);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return addressList;
     }
 
     @Override
